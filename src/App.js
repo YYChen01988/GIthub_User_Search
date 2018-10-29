a471145029b02893bc586d5c7e0b71f0fa2639c9
yingying <chen.yingying.01988@gmail.com>
2018-10-29 18:01:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      username:null,
+      id:null,
+      url: null,
+      avatar_url: null
+    }
+  }
+
+  getUser(username){
+    return fetch(`https://api.github.com/users/${username}`)
+    .then(response => response.json())
+    .then(response =>{
+      return response;
+    })
+  }
+
+  async handleSubmit(event){
+    event.preventDefault();
+    let user = await this.getUser(this.refs.username.value);
+    this.setState({
+      username: user.login,
+      id: user.id,
+      url: user.url,
+      avatar_url: user.avatar_url
+    });
+  }
+
+
   render() {
+    let user;
+    if(this.state.username){
+      const GitHubUrl = `https://github.com/${this.state.username}`
+      user = <div>
+        <p>Username: {this.state.username}</p>
+        <p>ID: {this.state.id}</p>
+        <p>Url: <a href={GitHubUrl}>GitHub Profile</a></p>
+        <img src={this.state.avatar_url}/>
+      </div>
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header"> <h1>GitHub Search</h1>
         </header>
+        <body>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input ref='username' type='text' placeholder = 'username'>
+          </input>
+        </form>
+        <p className = "App-intro">
+          {user}
+        </p>
+        </body>
       </div>
     );
   }
